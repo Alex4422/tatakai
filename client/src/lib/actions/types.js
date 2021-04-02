71d7e0662a03b90ace94588b9dc3c85192a2c73c
Makariudo <sspassevitch@gmail.com>
2021-04-02 16:43:55 +0200

    base template TS CRA/ delete client CRA

--- client/src/lib/actions/types.js
+++
@@ -1,7 +0,0 @@
-//types contract
-export const MINT_NFT_SUCCESS = "MINT_NFT_SUCCESS";
-export const TRANSACTION_ERROR = "TRANSACTION_ERROR";
-export const TRANSACTION_PENDING = "TRANSACTION__PENDING";
-
-export const LOG_EVENT = "LOG_EVENT";
-export const DEPLOY = "DEPLOY";
--- client/src/lib/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import contracts from "./contracts";
-import storage from "./storage";
-
-export default combineReducers({
-  storage,
-  contracts,
-  /* additional reducers here */
-});
