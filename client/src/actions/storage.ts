0aadff97463cfe7983a99ac8c3dc179defafcb0c
Sandy Instructor <sandyludosky@gmail.com>
2021-04-02 22:54:36 +0200

    edit redux state, update container and component

--- client/src/actions/storage.ts
+++ client/src/actions/storage.ts
@@ -1,11 +1,18 @@
 import { CHANGE_FIELD, SUBMIT_VALUE, SEED_VALUE_RECEIPT } from "./types";
 
-export const changeField = (field: string, value: Number) => ({
+interface IEventType {
+  target: {
+    name: string;
+    value: number;
+  };
+}
+export const changeField = ({ target: { name, value } }: IEventType) => ({
   type: CHANGE_FIELD,
-  payload: { [field]: value },
+  payload: { [name]: value },
 });
-export const submitValue = () => ({
+export const submitValue = ({ target: { value } }: IEventType) => ({
   type: SUBMIT_VALUE,
+  payload: { storageValue: value, inputValue: value },
 });
 export const seedValueReceipt = (value: Number) => ({
   type: SEED_VALUE_RECEIPT,
