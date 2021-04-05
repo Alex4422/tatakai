ca931700f5ed38e3a3d67ad0a9b7c3fe6cc31373
Sandy Instructor <sandyludosky@gmail.com>
2021-04-05 23:02:27 +0200

    edit routes and components

---
+++ client/src/components/MarketPlace/index.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { getAuthMetamask } from "../../lib/actions/user";
+import Container from "@material-ui/core/Container";
+import Gallery from "../../ui/Gallery";
+import CardsCarousel from "../../ui/CardsCarousel";
+import "../styles.css";
+
+declare interface Props {
+  getAuthMetamask: Function;
+  initMarket: Function;
+  nfts: Array<Object> | null;
+}
+
+const useStyles = () => {
+  const classes = {
+    input: {
+      padding: 5,
+    },
+    form: {
+      marginTop: 10,
+    },
+  };
+  return classes;
+};
+const MarketPlace = ({ getAuthMetamask, initMarket, nfts }: Props) => {
+  const classes = useStyles();
+  const [spacing, setSpacing] = useState(2);
+
+  useEffect(() => {
+    getAuthMetamask();
+    //initMarket()
+  }, []);
+
+  return (
+    <Container component="main" maxWidth="lg">
+      <div className="App">
+        <h1>Liste des NFTS : </h1>
+        {/* <CardsCarousel items={nfts} /> */}
+        <Gallery items={nfts} />
+      </div>
+    </Container>
+  );
+};
+
+export default MarketPlace;
--- client/src/containers/index.tsx
+++ client/src/containers/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./Home";
+import MarketPlace from "./MarketPlace";
 import Admin from "./Admin";
 import Navbar from "../ui/Navbar";
 
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar />
       <div className=" mt-4">
         <Route exact path="/" component={Home} />
+        <Route exact path="/marketplace" component={MarketPlace} />
         <Route path="/admin" component={Admin} />
       </div>
     </Router>
