f150553af9ecb9385816cc786f9026fd3baf4e44
Lorcann R <lorcann.rauzduel@platypus-agency.fr>
2021-04-07 20:36:07 +0200

    update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ var Faucet = artifacts.require("Faucet");
 var SimpleStorage = artifacts.require("SimpleStorage");
 
 module.exports = async (deployer) => {
-  await deployer.deploy(TakToken, 10000, "Tatakai", "TAK");
+  await deployer.deploy(TakToken, 1000000000000, "Tatakai", "TAK");
   await deployer.deploy(Marketplace, TakToken.address);
   await deployer.deploy(Faucet, TakToken.address);
   await deployer.deploy(CardItem, "TatakaiCard", "TAKCARD", Marketplace.address);
