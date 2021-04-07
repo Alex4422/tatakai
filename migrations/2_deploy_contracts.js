997342ebdb7fe015ab1133187b6eac8fed0c73d6
Lorcann R <lorcann.rauzduel@platypus-agency.fr>
2021-04-07 19:31:16 +0200

    add faucet contract deploy in migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,12 @@
 var TakToken = artifacts.require("TakToken");
 var Marketplace = artifacts.require("Marketplace");
 var CardItem = artifacts.require("CardItem");
+var Faucet = artifacts.require("Faucet");
 var SimpleStorage = artifacts.require("SimpleStorage");
 
 module.exports = async (deployer) => {
   await deployer.deploy(TakToken, 10000, "Tatakai", "TAK");
   await deployer.deploy(Marketplace, TakToken.address);
+  await deployer.deploy(Faucet, TakToken.address);
   await deployer.deploy(CardItem, "TatakaiCard", "TAKCARD", Marketplace.address);
 };
