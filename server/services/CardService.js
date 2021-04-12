6c25dfe561f534c7844c9a2df2951f801653aa42
Lorcann R <lorcann.rauzduel@platypus-agency.fr>
2021-04-13 01:42:23 +0200

    fix filter get cards on sale

--- server/services/CardService.js
+++ server/services/CardService.js
@@ -63,27 +63,30 @@ class CardService extends Service {
         try {
             const nft_count = await this.CardItemContract._tokenIds();
             let all_nfts = [];
-            for (let i = 1; i <= nft_count; i++) {
-            const owner = await this.CardItemContract.ownerOf(i);
-            const ipfsHash = await this.CardItemContract.tokenURI(i);
-            if(ipfsHash !== "") {
-                const info = await axios.get("https://ipfs.io/ipfs/"+ipfsHash);
-                const url = `https://api.pinata.cloud/data/pinList?metadata[keyvalues][isForSale]={"value":"1", "op":"eq"}`;
-                const nft_info = await axios.get(url, {
-                    headers: {
-                        pinata_api_key: process.env.PINATA_KEY, 
-                        pinata_secret_api_key: process.env.PINATA_SECRET_KEY,
-                    },
-                });
-                all_nfts.push({
-                    id: i,
-                    owner: owner,
-                    name: info.data.name,
-                    description: info.data.description,
-                    image: info.data.image,
-                    metadata: nft_info.data.rows[0] !== undefined ? nft_info.data.rows[0].metadata.keyvalues : null
-                });
-            }
+            for (let i = 1; i <= nft_count.toNumber(); i++) {
+                const owner = await this.CardItemContract.ownerOf(i);
+                const ipfsHash = await this.CardItemContract.tokenURI(i);
+                if(ipfsHash !== "") {
+                    const url = `https://api.pinata.cloud/data/pinList?metadata[keyvalues][isForSale]={"value":"1", "op":"eq"}`;
+        
+                    const nft_info = await axios.get(url, {
+                        headers: {
+                            pinata_api_key: process.env.PINATA_KEY, 
+                            pinata_secret_api_key: process.env.PINATA_SECRET_KEY,
+                        },
+                    });
+                    if(nft_info.data.rows[0].ipfs_pin_hash == ipfsHash) {
+                        const info = await axios.get("https://ipfs.io/ipfs/"+ipfsHash);
+                        all_nfts.push({
+                            id: i,
+                            owner: owner,
+                            name: info.data.name,
+                            description: info.data.description,
+                            image: info.data.image,
+                            metadata: nft_info.data.rows[0].metadata.keyvalues || null
+                        });
+                    }
+                }
             }
             return all_nfts;
         }
