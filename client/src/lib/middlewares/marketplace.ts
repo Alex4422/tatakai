8b672abba3568ea07d7c565739b427fca0c90b1b
Makariudo <sspassevitch@gmail.com>
2021-04-14 16:29:18 +0200

    to fix buy promise

--- client/src/lib/middlewares/marketplace.ts
+++ client/src/lib/middlewares/marketplace.ts
@@ -1,18 +1,16 @@
 import { INIT_MARKET, BUY_NFT, SELL_NFT, WITHDRAW_NFT_ON_SALE } from "../actions/types";
 import { seedMarket, buyNFTSuccess, isLoading } from "../actions/marketplace";
-import MarketplaceInstanceCall from "./utils/Marketplace";
-import CardItemInstanceCall from "./utils/cardItem";
-import TakTokenInstanceCall from "./utils/TakToken";
+import buy from "./utils/buy";
 import {API_URL} from "./utils/Constantes";
 import axios from "axios";
+import { resolveProjectReferencePath } from "typescript";
 
 
 const MarketplaceMW = () => ({ dispatch, getState }: any) => (
   next: any
 ) => (action: IAction) => {
   const {
-    user: { accounts, web3 },
-    contract: {TakToken, Marketplace, CardItem}
+    user: { accounts, web3 }
   } = getState();
 
   switch (action.type) {
@@ -44,32 +42,12 @@ const MarketplaceMW = () => ({ dispatch, getState }: any) => (
   /********************************/
     case BUY_NFT: {
       let data = { id: action.payload.id, price: action.payload.price };
-      try {
-        //TODO mutualiser ca!
-      let MarketplaceInstance: any;
-      let TakTokenInstance: any;
-      let CardItemInstance: any;
-
-      MarketplaceInstanceCall(web3).then(result => MarketplaceInstance = result);
-      TakTokenInstanceCall(web3).then(result => TakTokenInstance = result);
-      CardItemInstanceCall(web3).then(result => CardItemInstance = result);
-
-      TakTokenInstance.methods.approve(MarketplaceInstance._address,parseInt(data.price, 10)).send({from: accounts[0]}).then((result: any) => {
-        console.dir(result);
-        if(result){
-          console.log(CardItemInstance._address,accounts[0], data.id, data.price)
-           TakTokenInstance.methods.allowance(accounts[0], Marketplace.options.address).call({from: accounts[0]}).then((result: any) => {
-            console.log("allowance", result);
-          });
-           MarketplaceInstance.methods.buy(CardItemInstance._address, data.id, parseInt(data.price, 10)).send({from: accounts[0]}).then((response: any) => {
-            console.dir(response)
-            dispatch(buyNFTSuccess())
-          })
-        }
-      })
-      } catch (error) {
-        console.error(error);
-      }
+      buy(web3, accounts, data)
+        .then(res => {
+          console.log("coucou d'ici")
+          res ? dispatch(buyNFTSuccess()) : console.log("errror")
+        })
+        .catch(err => console.error(err))
       next(action)
       break;
     }
@@ -104,7 +82,7 @@ const MarketplaceMW = () => ({ dispatch, getState }: any) => (
       let data = {id}
       console.log("id", id)
       const config: Object = {
-        url: `${URL}cards/remove`,
+        url: `${API_URL}cards/remove`,
         method: 'post',
         headers: {
           "Content-Type": `application/json`,
