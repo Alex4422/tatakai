a807a884fa702b4548443ca4bac579ff3a50bcd7
Makariudo <sspassevitch@gmail.com>
2021-04-19 00:21:41 +0200

    homepage debut

--- client/src/components/Home/index.tsx
+++ client/src/components/Home/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import Container from "@material-ui/core/Container";
 import Modal from "../../ui/Modal";
-import img from "../../assets/pexels-pavel-danilyuk-6296034.jpg";
+import { makeStyles } from "@material-ui/core/styles";
+import combat from "../../assets/combat.png";
 import "../styles.css";
 
 declare interface Props {
@@ -10,14 +11,25 @@ declare interface Props {
   getUserStorage: Function;
   subscribeEvents: Function
 }
+
+const useStyles = makeStyles((theme) => ({
+
+  breadcrumbs: {
+    color: 'gray',
+    marginTop: '40px',
+    marginBottom: '40px'
+  },
+}));
+
+
 const Home = ({ getAuthMetamask, accounts, getUserStorage, subscribeEvents }: Props) => {
+  const classes = useStyles();
+  
   useEffect(() => {
     if(!accounts){
       getAuthMetamask();
     }
   }, [accounts]);
-
-
 
   useEffect(() => {
     getUserStorage();
@@ -27,8 +39,13 @@ const Home = ({ getAuthMetamask, accounts, getUserStorage, subscribeEvents }: Pr
   return (
     <Container component="main" maxWidth="lg" >
       <Modal />
-      <div className="App" >
-      {/*  */}
+      <div className="home" >
+      <h1 className="titre">TAT<span className="golden">A</span>KAI</h1>
+      <div className="textbox">
+        <p className="catch"><strong>Get</strong> your fighters</p>
+        <p className="catch"><strong>Bet</strong> with them</p>
+        <p className="catch golden "><strong>Win</strong> unique Card</p>
+      </div>
       </div>
     </Container>
   );
