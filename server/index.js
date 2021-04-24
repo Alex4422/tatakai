7889b7bcdf80a4fc737a2c8ba493d56f443e805b
Lorcann R <lorcann.rauzduel@platypus-agency.fr>
2021-04-24 02:48:11 +0200

    update server index.js

--- server/index.js
+++ server/index.js
@@ -19,8 +19,10 @@ app.use(express.urlencoded({ extended: true }));
 
 require("./config/routes")(app);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT, () => {
